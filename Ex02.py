f0377b2000cdc58207b0a290acb25037e9bc4e9a
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-01-09 20:59:15 -0300

    Create Ex02.py

---
+++ Ex02.py
@@ -0,0 +1,7 @@
+'''2- Calculadora de Soma
+* Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+'''
+
+n1 = 12
+n2 = 14
+print(f"A soma de {n1} e {n2} é igual a {n1+n2}")
