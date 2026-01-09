a39bde8b6db99e0098191fc27b8e57014988c4d3
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-01-09 20:59:50 -0300

    Adicionar calculadora de volume para caixa retangular
    Implementa um programa que calcula o volume de uma caixa retangular com dimensões fixas.

---
+++ Ex03.py
@@ -0,0 +1,16 @@
+'''3- Calculadora de Volume
+* Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+
+* Comprimento: 12 cm
+* Largura: 14 cm
+* Altura: 20 cm
+O programa deve calcular o volume e exibir o resultado em cm³.
+'''
+
+c = 12
+l = 14
+h = 20
+
+resultado = (c*l*h)
+print(f"O volume é {resultado:.2f}cm³")
+
