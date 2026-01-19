921503c816543e2ab7609c10ad20405e20fd6835
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-01-19 04:48:52 -0300

    Add age classification based on user input

---
+++ Ex09.py
@@ -0,0 +1,12 @@
+idade = int(input("Informe sua idade: "))
+
+if 0 <= idade <= 12:
+    print("Classificação: Criança")
+elif 13 <= idade <= 17:
+    print("Classificação: Adolescente")
+elif 18 <= idade <= 59:
+    print("Classificação: Adulto")
+elif idade >= 60:
+    print("Classificação: Idoso")
+else:
+    print("Idade inválida")
